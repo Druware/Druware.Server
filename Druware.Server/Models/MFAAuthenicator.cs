5d4122b7f6f3e3a5b08dbc74844deea3c002315f
Drufus <dru@openbcm.com>
2025-12-02 21:49:48 -0500

    Adding MFA Support Phase 1

---
+++ Druware.Server/Models/MFAAuthenicator.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Druware.Server.Models;
+
+public class MfaAuthenicator
+{
+    [Required(ErrorMessage = "Authenticator Code is required")]
+    public string? Code { get; set; }
+}
